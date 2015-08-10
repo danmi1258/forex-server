edaf45ab921e93a4eedb341bae5aaab2cf0296f2
sergey <spectrs@bk.ru>
2015-08-10 23:00:13 +0300

    fix middleware auth match for api/login path

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -2,7 +2,7 @@ var UnauthorizedError = require('../routes/httpErrors').Unauthorized;
 var LOGIN_PATH = '/api/login';
 
 module.exports.auth = function(req, res, next) {
-    req.isAuthenticated() || req.originalUrl === LOGIN_PATH ?
+    req.isAuthenticated() || req.originalUrl.indexOf(LOGIN_PATH) !== -1 ?
         next() :
         next(new UnauthorizedError());
 };
